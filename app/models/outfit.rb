d4aa051a0ae946564a5a32473ebd29d778b068bf
gpatuwo <grace.patuwo@gmail.com>
2013-12-16 15:24:20 -0800

    added validations

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -1,3 +1,5 @@
 class Outfit < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
+	mount_uploader :image, ImageUploader
+  validates :description, :gender, :image, :presence => true
+  validates :min_temp, :numericality => { :only_integer => true}
 end
