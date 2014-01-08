a47f83791a8e6aaa7466aa761a3e9549971f0dc9
gpatuwo <grace.patuwo@gmail.com>
2014-01-08 09:14:59 -0800

    attempted carrierwave/fog

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAIZNLS6DOX73ZBY2Q',
+    :aws_secret_access_key  => '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',
+    # :region => 'us-west-1' -- only if you're not in US Standard
+  }       
+  if Rails.env.development?
+    config.fog_directory = 'fw_app-dev'
+  else
+    config.fog_directory = 'fw_app'
+  end
+end
